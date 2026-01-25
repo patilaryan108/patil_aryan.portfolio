9db32d4fc2ba62bef5e9a1acdb1cdaa67f2287e1
patilAryan1008 <aspatil1081@gmail.com>
2026-01-25 12:50:05 +0530

    Complete overflow fix: hide scrollbar, optimize card sizing with max-width

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,12 +5,12 @@ import Card3 from './Card3'
 export default function Project() {
     return (
         <>
-            <div id='projects' style={{ maxWidth: "100%", boxSizing: "border-box", overflowX: "hidden" }}>
+            <div id='projects' style={{ maxWidth: "100vw", width: "100%", boxSizing: "border-box", overflowX: "hidden", padding: "0 10px" }}>
                 <div className="project-background" >
-                    <div style={{ display: "flex", width: "100%", justifyContent: "center" }}>
+                    <div style={{ display: "flex", width: "100%", justifyContent: "center", boxSizing: "border-box" }}>
                         <h2 style={{ border: "5px solid #000000", margin: "0px", height: "fit-content", width: "fit-content", padding: "10px 25px 10px 25px", margin: "50px 0px 50px 0px" }} >PROJECTS</h2>
                     </div>
-                    <div style={{ display: "flex", justifyContent: "center", margin: "45px 0px 80px 0px", flexWrap: "wrap", maxWidth: "100%", overflowX: "hidden" }}>
+                    <div style={{ display: "flex", justifyContent: "center", margin: "45px 0px 80px 0px", flexWrap: "wrap", maxWidth: "100%", overflowX: "hidden", boxSizing: "border-box", gap: "20px" }}>
                         <Card1 img={"Resume.png"} name="PORTFOLIO" ></Card1>
                         <Card3 video={"/video.mp4"} name={"COMING SOON"}></Card3>
                         <Card3 video={"/video.mp4"} name={"COMING SOON"}></Card3>
